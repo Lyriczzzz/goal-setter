68bf25237fa9426188b7b9ee2a637d0370e5db42
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-03-29 10:45:09 -0300

    feat: add home page and redirect user based on authentication status

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from './pages/layout'
+import { Home } from './pages/home'
 import { SignIn } from './pages/sign-in'
 
 export const router = createBrowserRouter([
@@ -7,6 +8,10 @@ export const router = createBrowserRouter([
     path: '/',
     element: <Layout />,
     children: [
+      {
+        path: '/',
+        element: <Home />
+      },
       {
         path: '/sign-in',
         element: <SignIn />
