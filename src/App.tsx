588a930f012c9ee68b07cae0fb023f15725af56e
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-03-29 21:12:23 -0300

    chore(eslint and prettier-plugin-tailwindcss): standardize code and remove arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from './components/theme-provider'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
-export const App = () => {
+export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
