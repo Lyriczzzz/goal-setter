5d2070f274fc73ab1f094f859ca654eb61993d9a
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-03-31 13:18:19 -0300

    feat: add link to return to your goals page

--- src/pages/create-goal/create-goal.tsx
+++ src/pages/create-goal/create-goal.tsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
+import { ArrowLeft } from 'lucide-react'
 import { CreateGoalForm } from './create-goal-form'
+import { Button } from '@/components/ui/button'
 
 export function CreateGoal() {
   return (
     <div className="container mx-auto flex flex-col gap-8 pb-24 pt-12">
+      <Button asChild variant="link" className="w-max">
+        <Link to="/your-goals">
+          <ArrowLeft className="mr-2 size-4" />
+          Back
+        </Link>
+      </Button>
       <h1 className="text-4xl font-bold">Create Goal</h1>
 
       <CreateGoalForm />
