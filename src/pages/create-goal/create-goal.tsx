681f6f8432b2a4cb0fd112ae2be7490e086464bb
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-04-04 15:13:47 -0300

    feat: change the button variable

--- src/pages/create-goal/create-goal.tsx
+++ src/pages/create-goal/create-goal.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 export function CreateGoal() {
   return (
     <div className="container mx-auto flex flex-col gap-8 pb-24">
-      <Button asChild variant="outline" className="w-max">
+      <Button asChild variant="secondary" className="w-max">
         <Link to="/your-goals">
           <ArrowLeft className="mr-2 size-4" />
           Back
